d55c07c952f12dfecc8c61e568981edc4e568bc7
mguevara717 <marissa.guevara717@gmail.com>
2018-09-05 10:19:12 -0400

    Done.

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -39,40 +39,4 @@ RSpec.describe AuthorsController do
       expect(response).to redirect_to(author_path(bro_found))
     end
   end
-
-  describe "creating an invalid author" do
-    let(:bad_attributes) do
-      {
-        email: @author.email,
-        phone_number: "555"
-      }
-    end
-
-    before { @bad = Author.create(bad_attributes) }
-
-    it "does not save" do
-      expect(@bad).to be_new_record
-    end
-
-    it "does not create" do
-      expect(Author.find_by(phone_number: bad_attributes[:phone_number])).to be_nil
-    end
-
-    it "has an error for missing name" do
-      expect(@bad.errors[:name]).to_not be_empty
-    end
-
-    it "has an error for non-unique email" do
-      expect(@bad.errors[:email]).to_not be_empty
-    end
-
-    it "has an error for too-short phone number" do
-      expect(@bad.errors[:phone_number]).to_not be_empty
-    end
-
-    it "renders the form again" do
-      post :create, bad_attributes
-      expect(response).to render_template(:new)
-    end
-  end
-end
+end 
